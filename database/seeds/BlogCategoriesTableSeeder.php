58da700424b96a08ecc743c7487d48c2d777755c
develr57 <develr57@mail.ru>
2020-02-21 01:30:25 +0500

    3

---
+++ database/seeds/BlogCategoriesTableSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class BlogCategoriesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $categories = [];
+
+        $cName = 'Без категории!';
+        $categories[] = [
+            'title'     => $cName,
+            'slug'      => str_slug($cName),
+            'parent_id' => 0,
+        ];
+
+        for ($i = 1; $i <= 10; $i++)
+        {
+            
+        }
+    }
+}
