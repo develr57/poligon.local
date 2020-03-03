2d11557454307070c594471fba026ecedfa2b4eb
SysAdmin <C:\Users\Admin\AppData\Roaming\The Bat!>
2020-03-03 16:48:40 +0500

    2

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -11,6 +11,19 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $data = [
+            [
+                'name'      =>  'Автор не известен',
+                'email'     =>  'Autor_unknown@g.g',
+                'password'  =>  bcrypt(str_random(16)),
+            ],
+            [
+                'name'      =>  'Автор',
+                'email'     =>  'Autor1@g.g',
+                'password'  =>  bcrypt(str_random('123123')),
+            ],
+        ];
+
+        DB::table('users')->insert($data);
     }
 }
